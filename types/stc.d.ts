850368479e0780b3626ef7e59293b3ec7d652eeb
Raphael Tang <raphpb1912@gmail.com>
2022-03-15 13:49:32 +0800

    added some JSDoc comments to let users know what the type is exactly

--- types/stc.d.ts
+++ types/stc.d.ts
@@ -1,8 +1,35 @@
+/**
+ * Signed 16 bit integer
+ */
 type i16 = number;
+
+/**
+ * Signed 32 bit integer
+ */
 type i32 = number;
+
+/**
+ * Signed 64 bit integer
+ */
 type i64 = number;
+
+/**
+ * Signed 16 bit float
+ */
 type f16 = number;
+
+/**
+ * Signed 32 bit float
+ */
 type f32 = number;
+
+/**
+ * Signed 64 bit float
+ */
 type f64 = number;
 
+/**
+ * Char type,
+ * takes up a single byte in memory
+ */
 type char = string;
